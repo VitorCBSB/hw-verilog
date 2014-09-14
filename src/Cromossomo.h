888d6de4c36871f7ff736ccfd9eb2a04be392445
Vitor Coimbra <vitorcoliveira91@gmail.com>
2014-09-13 22:07:39 -0300

    Construtor padrao constroi bitsets aleatoriamente agora.

--- src/Cromossomo.h
+++ src/Cromossomo.h
@@ -10,6 +10,7 @@
 
 #include <bitset>
 #include <array>
+#include <random>
 #include <math.h>
 
 #define BITS_TERMINAIS (int) ceil((log(R * C + NumIn) / log(2)))
@@ -17,12 +18,36 @@
 
 template<int NumIn, int NumOut, int R, int C>
 class Cromossomo {
-private:
+public:
 	std::array<std::bitset<SAIDAS_LUT + BITS_TERMINAIS * 2>, R * C> elementos_logicos;
 	std::array<std::bitset<BITS_TERMINAIS>, NumOut> saidas;
 
 public:
-	Cromossomo();
+	Cromossomo() {
+		for (int i = 0; i < R * C; i++) {
+			elementos_logicos[i] = aleatorio<SAIDAS_LUT + BITS_TERMINAIS * 2>();
+		}
+		for (int i = 0; i < NumOut; i++) {
+			saidas[i] = aleatorio<BITS_TERMINAIS>();
+		}
+	}
+
+	void gerar_filho(
+			const Cromossomo<NumIn, NumOut, R, C>& outro_pai) {
+
+	}
+
+private:
+	template<int tamanho>
+	std::bitset<tamanho> aleatorio() {
+		std::random_device rd;
+		std::bitset<tamanho> resultado;
+
+		for (int i = 0; i < tamanho; i++) {
+			resultado[i] = rd() % 2;
+		}
+		return resultado;
+	}
 };
 
 #endif /* CROMOSSOMO_H_ */
