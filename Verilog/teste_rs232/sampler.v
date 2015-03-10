c2185e34235bb02f25130b2c1ce2528e78afdac2
Vitor Coimbra <vitorcoimbra6@hotmail.com>
2015-03-10 18:42:48 -0300

    Sampler arrumado e pronto para ser utilizado... em teoria.

--- Verilog/teste_rs232/sampler.v
+++ Verilog/teste_rs232/sampler.v
@@ -1,38 +1,70 @@
-module sampler(iClock, iStartSignal, oWriteEnable, oAddress, oFinished);
+module sampler(iClock, iStartSignal, oAddress, oFinished);
+
+	parameter IDLE = 2'b00,
+		SAMPLING = 2'b01,
+		INCREMENTING_ADDR = 2'b10,
+		FINISHED_SAMPLING = 2'b11;
 
 	input iClock;
 	input iStartSignal;
-	output reg oWriteEnable;
 	output reg [15:0] oAddress;
-	output reg oFinished;
+	output oFinished;
 	
+	reg [1:0] state;
 	reg started;
 	integer counter;
 
-initial begin
-	oFinished <= 0;
-	oWriteEnable <= 0;
-	oAddress <= 16'b0;
-	counter <= 20'b0;
-end
+	function [1:0] next_state;
+		input [1:0] state;
+		input start_signal;
+		input [15:0] address;
+		
+		case(state)
+		IDLE:
+			if (start_signal) begin
+				next_state = SAMPLING;
+			end else begin
+				next_state = IDLE;
+			end
+		SAMPLING:
+			if (address < 16'hFFFF) begin
+				next_state = FINISHED_SAMPLING;
+			end else begin
+				next_state = INCREMENTING_ADDR;
+			end
+		INCREMENTING_ADDR: 
+			next_state = SAMPLING;
+		FINISHED_SAMPLING:
+			next_state = IDLE;
+		default:
+			next_state = IDLE;
+		endcase
 	
-always @(iClock) begin
-	if (oFinished)
-		oFinished <= 0;
-	if (iStartSignal) begin
-		counter <= 20'b0;
-		oWriteEnable <= 1;
-		started <= 1;
+	endfunction
+	
+always@(posedge iClock) begin
+	if (iReset) begin
+		state <= IDLE;
+	end else begin
+		state <= next_state;
 	end
-	if (started) begin
-		if (counter >= 20'h0FFFF) begin // Condição de término de amostragem
-			started <= 0;
-			oWriteEnable <= 0;
-			oFinished <= 1;
-		end
-		counter <= counter + 1;
+end
+
+always@(posedge iClock) begin
+	oFinished <= 0;
+	case(state)
+	IDLE: begin
+		oAddress <= 16'b0;
 	end
-	oAddress <= counter;
+	SAMPLING: begin
+	end
+	INCREMENTING_ADDR: begin
+		oAddress <= oAddress + 1;
+	end
+	FINISHED_SAMPLING: begin
+		oFinished <= 1;
+	end
+	endcase
 end
 
 endmodule
