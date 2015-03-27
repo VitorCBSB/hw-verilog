5a8933cb356ce301de2cc7d2b9525b9e4dd5eaff
Vitor Coimbra <vitorcoliveira91@gmail.com>
2015-03-27 13:33:59 -0300

    Adicionada implementação da função de fitness.

--- C++/src/hw-verilog.cpp
+++ C++/src/hw-verilog.cpp
@@ -18,8 +18,17 @@ int main() {
 	std::mt19937 mt;
 	mt.seed(time(nullptr));
 	Populacao<20, 4, 1, 4, 5, 5> populacao(mt, true, new IcarusFitnessCalculator(4, 1,
-			[](const std::vector<std::vector<std::bitset<8>>>&) -> double {
-		return 0.0;
+			[](const std::vector<std::vector<std::bitset<8>>>& individual_output)
+			-> double {
+		int num_acertos = 0;
+		for (unsigned int i = 0; i < 15; i++) {
+			num_acertos += individual_output[i][0] == 0;
+		}
+		num_acertos += individual_output[15][0] == 1;
+		if (num_acertos == 16) {
+			return 200000000.0;
+		}
+		return 1.0 / (double) num_acertos;
 	}));
 
 	return 0;
