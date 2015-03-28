58eb9d12218211bb95eff4eb72e0cf959071f9b1
Vitor Coimbra <vitorcoliveira91@gmail.com>
2015-03-28 12:29:22 -0300

    Implementado main loop do algoritmo genético.

--- C++/src/hw-verilog.cpp
+++ C++/src/hw-verilog.cpp
@@ -15,11 +15,12 @@
 #include "IcarusFitnessCalculator.h"
 
 int main() {
+	const double MELHOR_FITNESS = 20000000.0;
 	std::mt19937 mt;
 	mt.seed(time(nullptr));
-	Populacao<20, 4, 1, 4, 2, 2> populacao(mt, true,
+	Populacao<20, 4, 1, 4, 5, 5> populacao(mt, true,
 			new IcarusFitnessCalculator(4, 1,
-					[](const std::vector<std::vector<std::bitset<8>>>& individual_output)
+					[&](const std::vector<std::vector<std::bitset<8>>>& individual_output)
 					-> double {
 						int num_acertos = 0;
 						for (unsigned int i = 0; i < 15; i++) {
@@ -27,12 +28,17 @@ int main() {
 						}
 						num_acertos += individual_output[15][0] == 1;
 						if (num_acertos == 16) {
-							return 200000000.0;
+							return MELHOR_FITNESS;
 						}
 						return 1.0 / (double) num_acertos;
 					}));
 
-	populacao.proxima_geracao();
+	int geracao = 0;
+	while (populacao.melhor_individuo().fitness() != MELHOR_FITNESS) {
+		populacao.proxima_geracao();
+		std::cout << ++geracao << ": " <<
+				populacao.melhor_individuo().fitness() << std::endl;
+	}
 
 	populacao.melhor_individuo().criar_arquivo_verilog("melhor.v");
 
