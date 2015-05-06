d6a13d8382dd99bac640e22a8aeb13fcbbd6133e
Vitor Coimbra <vitorcoliveira91@gmail.com>
2015-05-06 11:33:45 -0300

    Novo projeto feito em C puro adicionado.

---
+++ C/src/hw-verilog-c.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : hw-verilog-c.c
+ Author      : Vitor Coimbra de Oliveira
+ Version     :
+ Copyright   : MIT
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("hello"); /* prints hello */
+	return EXIT_SUCCESS;
+}
