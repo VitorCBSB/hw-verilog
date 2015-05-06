f53af4d64bd804de2299104d0c0b14c860adf01a
Vitor Coimbra <vitorcoliveira91@gmail.com>
2015-05-06 18:41:11 -0300

    Primeira versão do cromossomo em C.

---
+++ C/src/cromossomo.h
@@ -0,0 +1,17 @@
+/*
+ * cromossomo.h
+ *
+ *  Created on: 06/05/2015
+ *      Author: vitor
+ */
+
+#ifndef CROMOSSOMO_H_
+#define CROMOSSOMO_H_
+
+typedef struct _cromossomo {
+	char le_defs[100];
+	char outs[10];
+} cromossomo;
+
+
+#endif /* CROMOSSOMO_H_ */
