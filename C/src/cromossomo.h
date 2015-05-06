d51dedc06daf191d59470d1ff5f50f335febe722
Vitor Coimbra <vitorcoliveira91@gmail.com>
2015-05-06 18:51:24 -0300

    Adicionado comentário explicando o cromossomo.

--- C/src/cromossomo.h
+++ C/src/cromossomo.h
@@ -8,8 +8,20 @@
 #ifndef CROMOSSOMO_H_
 #define CROMOSSOMO_H_
 
+/*
+ * Cada byte da le_defs e responsavel por um
+ * campo distinto do elemento logico, dentre eles:
+ *
+ * 	- Funcao
+ * 	- Entradas
+ *
+ * Para cada elemento. Ou seja, se o le tem
+ * 2 entradas, os bytes ocupados serao 3.
+ *
+ * 	1 para funcao + 2 para entradas
+ */
 typedef struct _cromossomo {
-	char le_defs[100];
+	char le_defs[300];
 	char outs[10];
 } cromossomo;
 
