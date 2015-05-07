37ba0e725e00d55f088e45ce066e672bd68baf2f
Vitor Coimbra <vitorcoliveira91@gmail.com>
2015-05-07 17:42:41 -0300

    Algoritmo genético principal feito na função main.

--- C/src/hw-verilog-c.c
+++ C/src/hw-verilog-c.c
@@ -8,10 +8,27 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "cromossomo.h"
+#include "populacao.h"
+#include "fitness_func.h"
+#include "selecao.h"
 
 int main(void) {
-	puts("hello"); /* prints hello */
-	return EXIT_SUCCESS;
+	genetic_params params;
+	params.r = 2;
+	params.c = 2;
+	params.feed_forward = 1;
+	params.num_in = 2;
+	params.num_out = 3;
+	params.num_funcs = 3;
+	params.le_num_in = 2;
+
+	populacao populacao = populacao_aleatoria(params, 5);
+	avaliar_individuos(params, &populacao);
+	while (melhor_individuo(&populacao).fitness != MAX_FITNESS) {
+		populacao = selecao_one_plus_lambda(params, &populacao);
+		avaliar_individuos(params, &populacao);
+	}
+
+	return 0;
 }
