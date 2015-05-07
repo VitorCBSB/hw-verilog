d2fc35c45cf692cdf96e3038035fa1c98cd05c94
Vitor Coimbra <vitorcoliveira91@gmail.com>
2015-05-07 18:12:59 -0300

    Alterada implementação do gerador de números aleatórios para mersenne twister, mais robusto que o anterior.

--- C/src/random.h
+++ C/src/random.h
@@ -10,7 +10,7 @@
 
 #define RAND_MAX ((1U << 31) - 1)
 
-unsigned int _random_();
-void _srandom_(unsigned int seed);
+unsigned long _random_();
+void _srandom_(unsigned long seed);
 
 #endif /* RANDOM_H_ */
