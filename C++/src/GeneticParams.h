8f0186ab40228152b85ae3d1609e3cbcd27900ad
Vitor Coimbra de Oliveira <vitorcoliveira91@gmail.com>
2015-05-08 16:27:20 -0300

    Nova classe carroca pra colocar todos os parametros geneticos.

---
+++ C++/src/GeneticParams.h
@@ -0,0 +1,28 @@
+/*
+ * GeneticParams.h
+ *
+ *  Created on: 08/05/2015
+ *      Author: Vitor
+ */
+
+#ifndef GENETICPARAMS_H_
+#define GENETICPARAMS_H_
+
+class GeneticParams {
+public:
+	unsigned int num_in;
+	unsigned int num_out;
+	unsigned int le_num_in;
+	unsigned int r;
+	unsigned int c;
+	bool feed_forward;
+
+	GeneticParams(unsigned int num_in, unsigned int num_out,
+			unsigned int le_num_in, unsigned int r, unsigned int c,
+			bool feed_forward) :
+			num_in(num_in), num_out(num_out), le_num_in(le_num_in), r(r), c(c), feed_forward(
+					feed_forward) {
+	}
+};
+
+#endif /* GENETICPARAMS_H_ */
