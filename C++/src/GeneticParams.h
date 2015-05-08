84d60b0329f37a0496d1cf018fd7479fe9f39311
Vitor Coimbra de Oliveira <vitorcoliveira91@gmail.com>
2015-05-08 18:39:56 -0300

    Colocado parametro de numero de funcoes em GeneticParams.

--- C++/src/GeneticParams.h
+++ C++/src/GeneticParams.h
@@ -15,13 +15,14 @@ public:
 	unsigned int le_num_in;
 	unsigned int r;
 	unsigned int c;
+	unsigned int num_funcs;
 	bool feed_forward;
 
 	GeneticParams(unsigned int num_in, unsigned int num_out,
-			unsigned int le_num_in, unsigned int r, unsigned int c,
-			bool feed_forward) :
-			num_in(num_in), num_out(num_out), le_num_in(le_num_in), r(r), c(c), feed_forward(
-					feed_forward) {
+			unsigned int num_funcs, unsigned int le_num_in, unsigned int r,
+			unsigned int c, bool feed_forward) :
+			num_in(num_in), num_out(num_out), num_funcs(num_funcs), le_num_in(
+					le_num_in), r(r), c(c), feed_forward(feed_forward) {
 	}
 };
 
