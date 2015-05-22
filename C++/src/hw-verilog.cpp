72982f330b3e96cb55d42bbeafc0b8c38db523fc
Vitor Coimbra de Oliveira <vitorcoliveira91@gmail.com>
2015-05-21 21:42:34 -0300

    Mudanças principais:
    	1. Agora utiliza MC-CGP. MC-CGP constitui em dividir cada bit da saída em uma evolução própria. Neste caso, cada bit é uma Populacao própria.

--- C++/src/hw-verilog.cpp
+++ C++/src/hw-verilog.cpp
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <functional>
 #include "Cromossomo.h"
 #include "Populacao.h"
 #include "IcarusFitnessCalculator.h"
@@ -23,23 +24,79 @@
 
 const double MELHOR_FITNESS = 20000000.0;
 const unsigned int NUM_SAMPLES = 100;
-const unsigned int NUM_IN = 3;
-const unsigned int NUM_OUT = 2;
-const unsigned int NUM_ROWS = 3;
-const unsigned int NUM_COLS = 3;
+const unsigned int NUM_IN = 4;
+const unsigned int NUM_OUT = 1;
+const unsigned int NUM_ROWS = 1;
+const unsigned int NUM_COLS = 10;
 const unsigned int LE_NUM_IN = 2;
 const bool FEED_FORWARD = true;
 
-double fitness(const std::vector<std::vector<std::bitset<8>>>& individual_output) {
+double fitness0(const std::vector<std::vector<std::bitset<8>>>& individual_output) {
 	int soma_distancias = 0;
-	soma_distancias += individual_output[0][0].to_ulong();
+	soma_distancias += abs((int) individual_output[0][0].to_ulong() - 0);
 	soma_distancias += abs((int) individual_output[1][0].to_ulong() - 1);
-	soma_distancias += abs((int) individual_output[2][0].to_ulong() - 1);
-	soma_distancias += abs((int) individual_output[3][0].to_ulong() - 2);
+	soma_distancias += abs((int) individual_output[2][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[3][0].to_ulong() - 1);
 	soma_distancias += abs((int) individual_output[4][0].to_ulong() - 1);
-	soma_distancias += abs((int) individual_output[5][0].to_ulong() - 2);
-	soma_distancias += abs((int) individual_output[6][0].to_ulong() - 2);
-	soma_distancias += abs((int) individual_output[7][0].to_ulong() - 3);
+	soma_distancias += abs((int) individual_output[5][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[6][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[7][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[8][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[9][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[10][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[11][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[12][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[13][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[14][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[15][0].to_ulong() - 0);
+	if (soma_distancias == 0) {
+		return MELHOR_FITNESS;
+	}
+	return 1.0 / ((double) soma_distancias);
+}
+
+double fitness1(const std::vector<std::vector<std::bitset<8>>>& individual_output) {
+	int soma_distancias = 0;
+	soma_distancias += abs((int) individual_output[0][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[1][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[2][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[3][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[4][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[5][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[6][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[7][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[8][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[9][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[10][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[11][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[12][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[13][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[14][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[15][0].to_ulong() - 1);
+	if (soma_distancias == 0) {
+		return MELHOR_FITNESS;
+	}
+	return 1.0 / ((double) soma_distancias);
+}
+
+double fitness2(const std::vector<std::vector<std::bitset<8>>>& individual_output) {
+	int soma_distancias = 0;
+	soma_distancias += abs((int) individual_output[0][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[1][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[2][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[3][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[4][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[5][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[6][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[7][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[8][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[9][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[10][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[11][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[12][0].to_ulong() - 0);
+	soma_distancias += abs((int) individual_output[13][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[14][0].to_ulong() - 1);
+	soma_distancias += abs((int) individual_output[15][0].to_ulong() - 1);
 	if (soma_distancias == 0) {
 		return MELHOR_FITNESS;
 	}
@@ -62,24 +119,36 @@ int main(int argc, char* argv[]) {
 	std::mt19937 mt;
 	mt.seed(time(nullptr));
 
-	Populacao populacao(
-			new OnePlusLambdaEvoStrategy(mt, 5, genetic_params,
-					new SimulationFitnessCalculator(genetic_params, fitness)));
+	std::vector<
+			std::function<double(const std::vector<std::vector<std::bitset<8>>>&)>>
+	funcoes_fitness = {fitness0, fitness1, fitness2};
+	std::vector<Populacao> populacoes;
 
-	int geracao = 0;
-	populacao.calcular_fitness();
-	while (geracao < 10000
-			&& populacao.melhor_individuo().fitness() != MELHOR_FITNESS) {
-		std::cout << geracao << ": " << populacao.melhor_individuo().fitness()
-				<< ", " << populacao.fitness_medio() << std::endl;
-		populacao.proxima_geracao();
-		populacao.calcular_fitness();
-		geracao++;
+	for (auto& funcao_fitness : funcoes_fitness) {
+		populacoes.push_back(
+				Populacao(
+						new OnePlusLambdaEvoStrategy(mt, 5, genetic_params,
+								new SimulationFitnessCalculator(genetic_params,
+										funcao_fitness))));
 	}
 
-	CriadorArquivos::cria_arquivo_top_icarus(genetic_params, populacao.melhor_individuo(), "melhor.v");
-	if (populacao.melhor_individuo().fitness() == MELHOR_FITNESS) {
-		std::cout << "Solucao encontrada na geracao " << geracao << std::endl;
+	for (auto& populacao : populacoes) {
+		int geracao = 0;
+		populacao.calcular_fitness();
+		while (geracao < 10000
+				&& populacao.melhor_individuo().fitness() != MELHOR_FITNESS) {
+			std::cout << geracao << ": "
+					<< populacao.melhor_individuo().fitness() << ", "
+					<< populacao.fitness_medio() << std::endl;
+			populacao.proxima_geracao();
+			populacao.calcular_fitness();
+			geracao++;
+		}
+		CriadorArquivos::cria_arquivo_top_icarus(genetic_params,
+				populacao.melhor_individuo(), "melhor.v");
+		if (populacao.melhor_individuo().fitness() == MELHOR_FITNESS) {
+			std::cout << "Solucao encontrada na geracao " << geracao << std::endl;
+		}
 	}
 
 	return 0;
