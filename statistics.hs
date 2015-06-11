428709e4dde0662f66cc33cb5d13694f8076f278
Vitor Coimbra <vitorcoliveira91@gmail.com>
2015-06-11 12:16:17 -0300

    Alterado para novo padrão de colhimento. Agora marca também o tempo de cada amostragem em segundos.

--- statistics.hs
+++ statistics.hs
@@ -3,6 +3,7 @@ import System.Directory
 import System.Environment
 import Data.List
 import Data.Maybe
+import Data.Time.Clock
 import Control.Monad
 import System.IO
 
@@ -12,19 +13,22 @@ chamarPrograma input =
 		output <- readProcess "/home/vitor/hw-verilog/C++/Debug/hw-verilog" [show input] ""
 		return (map read (words output) :: [Integer])
 
-chamarProgramaNVezes :: Integer -> Integer -> IO [[Integer]]
+chamarProgramaNVezes :: Integer -> Integer -> IO [(Integer, [Integer])]
 chamarProgramaNVezes n input =
 	if n == 0 then
 		return []
 	else
 		do 
                         print n
+			tempoInicial <- getCurrentTime
 			listOutput <- chamarPrograma input
+			tempoFinal <- getCurrentTime
+			let tempoExecucao = floor $ realToFrac $ tempoFinal `diffUTCTime` tempoInicial
 			case listOutput of
 				[-1] -> chamarProgramaNVezes n input
 				_ -> do
 					recursiveCall <- chamarProgramaNVezes (n - 1) input
-					return $ listOutput : recursiveCall
+					return $ (tempoExecucao, listOutput) : recursiveCall
 
 media [] = Nothing
 media lista = Just $ sum lista / fromIntegral (length lista)
@@ -39,8 +43,8 @@ desvioPadrao lista = let
 formatarMaybe (Just x) = show x
 formatarMaybe Nothing = "N/A"
 
-calcularEstatisticas :: Integer -> Integer -> [[Float]] -> [Maybe Float]
-calcularEstatisticas amostras max_geracoes listaAmostras = let 
+calcularEstatisticas :: [[Float]] -> [Maybe Float]
+calcularEstatisticas listaAmostras = let 
 	mediaConvergencia = media $ map (!! 0) listaAmostras
 	mediaNumPortasPre = media $ map (!! 1) listaAmostras
 	mediaNivelGatePre = media $ map (!! 2) listaAmostras
@@ -52,7 +56,7 @@ calcularEstatisticas amostras max_geracoes listaAmostras = let
 	taxaMelhoraNivelGate = liftM2 (/) mediaNivelGatePre mediaNivelGatePos
 	taxaMelhoraNumTrans = liftM2 (/) mediaNumTransPre mediaNumTransPos
 	in
-	[Just (fromInteger amostras), Just (fromInteger max_geracoes), mediaConvergencia, 
+	[mediaConvergencia, 
 			mediaNumPortasPre, mediaNivelGatePre, mediaNumTransPre, 
 			mediaNumPortasPos, mediaNivelGatePos, mediaNumTransPos,
 			taxaMelhoraNumPortas, taxaMelhoraNivelGate]
@@ -63,18 +67,37 @@ linhas n = replicate n '-'
 
 realMain :: [String] -> IO ()
 realMain [geracoesArg, amostrasArg] = do
+	let informacoesGerais = formatar ["Geracoes", "Amostras"]
+	let cabecalho = formatar $ ["Amostra", "Geracoes conv."]
+		++ ["N. portas (pre)", "G. path (pre)", "N. trans (pre)"]
+		++ ["N. portas (pos)", "G. path (pos)", "N. trans (pos)"]
+		++ ["Tempo (s)"]
+	let cabecalhoSumario = formatar $ ["Media convergencia"] 
+		++ ["Media n. portas (pre)", "Media g. path (pre)", "Media n. trans (pre)"] 
+		++ ["Media n. portas (pos)", "Media g. path (pos)", "Media n. trans (pos)"]
+		++ ["% n. portas", "% gate", "% trans"]
 	arquivoResultado <- openFile "resultado.txt" WriteMode
-	let cabecalho = formatar $ ["Amostras", "Max ger. otimizicao", "Media convergencia"] 
-		++ ["Media n. portas (pre)", "Media gate path (pre)", "Media num trans (pre)"] 
-		++ ["Media n. portas (pos)", "Media gate path (pos)", "Media num trans (pos)"]
-		++ ["% n. portas", "% gate"]
-	hPutStrLn arquivoResultado cabecalho
-	hPutStrLn arquivoResultado $ linhas (length cabecalho)
 	setCurrentDirectory "/home/vitor/hw-verilog/C++"
+
+	hPutStrLn arquivoResultado informacoesGerais
+	hPutStrLn arquivoResultado $ linhas (length informacoesGerais)
+	hPutStrLn arquivoResultado $ formatar [geracoesArg, amostrasArg]
+	hPutStrLn arquivoResultado ""
+
 	let maxGeracoes = read geracoesArg
 	let amostras = read amostrasArg
 	resultados <- chamarProgramaNVezes amostras maxGeracoes
-	let estatisticas = calcularEstatisticas amostras maxGeracoes ((map . map) fromInteger resultados)
+
+	hPutStrLn arquivoResultado cabecalho
+	hPutStrLn arquivoResultado $ linhas (length cabecalho)
+	mapM_ 
+		(\(amostra, (tempo, amostras)) -> hPutStrLn arquivoResultado $ formatar $ map show $ [amostra] ++ amostras ++ [tempo]) 
+		(zip [1..] resultados)
+	hPutStrLn arquivoResultado ""
+
+	let estatisticas = calcularEstatisticas (map (map fromInteger . snd) resultados)
+	hPutStrLn arquivoResultado cabecalhoSumario
+	hPutStrLn arquivoResultado $ linhas (length cabecalhoSumario)
 	hPutStrLn arquivoResultado $ formatar $ map formatarMaybe estatisticas
 	hClose arquivoResultado
 
